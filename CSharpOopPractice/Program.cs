cf36e5d21c09b32d221fa29504743ef8bad156b4
cartersw <carterwildenradt@gmail.com>
2026-02-28 01:42:47 -0500

    parse method

--- CSharpOopPractice/Program.cs
+++ CSharpOopPractice/Program.cs
@@ -8,15 +8,22 @@
         {
             Console.WriteLine("Hi {0}, I am {1}", to, Name);
         }
+
+        public static Person Parse(string str)
+        {
+            Person person = new Person();
+            person.Name = str;
+
+            return person;
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            person.Name = "test";
-            
+            Person person = Person.Parse("test");
+            person.Introduce("guy");
         }
     }
 }
