5e41b1acc1efbe0dd702a8f6f9593d3a8ddb3fbe
cartersw <carterwildenradt@gmail.com>
2026-03-01 02:46:40 -0500

    examples of oveloading methods

---
+++ CSharpOopPractice/Point.cs
@@ -0,0 +1,19 @@
+namespace CSharpIntermediate
+{
+    public class Point
+    {
+        public void Move(int x, int y)
+        {
+            
+        }
+        public void Move(Point newLocation)
+        {
+            
+        }
+        public void Move(Point newLocation, int speed)
+        {
+            
+        }
+
+    }
+}
