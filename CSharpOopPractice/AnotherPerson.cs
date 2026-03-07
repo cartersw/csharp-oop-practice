cc70f4dbd623d85ac33f0833daca4b2173a35ad2
cartersw <carterwildenradt@gmail.com>
2026-03-07 04:26:43 -0500

    anotherperson class

---
+++ CSharpOopPractice/AnotherPerson.cs
@@ -0,0 +1,13 @@
+namespace CSharpIntermediate
+{
+    public class AnotherPerson
+    {
+        public DateTime Birthdate { get; set; }
+
+        public string Name { get; set; } = "";
+
+        
+        
+    }
+
+}
