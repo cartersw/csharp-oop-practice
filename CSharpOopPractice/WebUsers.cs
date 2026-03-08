6a8431f61f01317060c661874b5c842492f82246
cartersw <carterwildenradt@gmail.com>
2026-03-08 05:28:33 -0400

    leave method webuser

--- CSharpOopPractice/WebUsers.cs
+++ CSharpOopPractice/WebUsers.cs
@@ -3,12 +3,17 @@ namespace CSharpIntermediate
     public class WebUsers
     {
         private Dictionary<string, string> _dictionary;
-        
+        public string Id { get; private set; } = "";
 
         public WebUsers()
         {
             _dictionary = new Dictionary<string, string>();
 
+        }
+
+        public void Leave()
+        {
+            _dictionary.Remove(Id);
         }
 
         public string this[string key]
